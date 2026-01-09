f259272faca042cb2a36ae3c2e29174b83fe5acf
Sam11dot <samruddhichaudhari185@gmail.com>
2026-01-09 15:29:35 +0530

    updated

--- app/Campioninfo/page.js
+++ app/Campioninfo/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
-
+import { Suspense } from "react";
 import ContactFormDesign from "../Compoent/Campionform";
 
 export default function campaioninfo() {
   return (
     <>
+       <Suspense fallback={<div className="text-center mt-5">Loading...</div>}>
 <ContactFormDesign />
+</Suspense>
     
     </>
    
